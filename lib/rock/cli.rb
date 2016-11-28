a7d609b2bca518278756fb5e6048385a51c02079
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-11-28 10:23:39 -0200

    bin: improve rock-hz's UI tremendously
    This adds a few features to rock-hz:
     - without arguments, or if the given task/port do not match an existing
       one, the script now lists matching tasks/ports and lets the user
       choose one. This is currently buggy if there is more tasks than
       one page, but IMO it's still much better than the current situation
       of having to figure out task and port names and type them.
     - the display uses the TTY gem instead of hardcoding ANSI
       codes, and formats the output as a table without having a running
       console
     - with the -c COUNT option, it shows a history of the values

---
+++ lib/rock/cli.rb
@@ -0,0 +1,46 @@
+require 'tty-prompt'
+
+module Rock
+    module CLI
+        def self.choose_orocos_task_and_port(name_service: Orocos.name_service, task_name: nil, port_name: nil)
+            prompt = TTY::Prompt.new
+
+            if task_name
+                begin
+                    selected_task = Orocos.get(task_name)
+                rescue Orocos::NotFound
+                end
+                candidates = name_service.names.grep(/(^|\/)#{task_name}/)
+                if candidates.empty?
+                    STDERR.puts "No task matches #{task_name}, and none start with #{task_name}"
+                    return
+                end
+                if candidates.size == 1
+                    task_name = candidates.first
+                else
+                    task_name = prompt.select("Select a task", candidates)
+                end
+            end
+
+            if !task_name
+                tasks = name_service.names
+                if tasks.empty?
+                    STDERR.puts "No tasks available"
+                    return
+                end
+                task_name = prompt.select("Select a task", tasks)
+            end
+            selected_task ||= Orocos.get(task_name)
+
+            if !port_name
+                ports = selected_task.each_port.
+                    find_all { |p| p.respond_to?(:reader) }
+                ports = Hash[ports.map { |p| [p.name, p] }]
+                prompt.select("Select a port", ports)
+            else
+                selected_task.port(port_name)
+            end
+        end
+    end
+end
+
