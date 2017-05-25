4a70961106f61a24b4d92ea43942d099a9582e56
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-05-25 09:37:48 -0300

    release: fix 'freeze' and 'unfreeze'

--- lib/rock/cli/release.rb
+++ lib/rock/cli/release.rb
@@ -16,6 +16,8 @@ module Rock
         #    overrides.d/80-release.packages.yml: version information for the packages
         #
         class Release < Thor
+            class_option :verbose, type: :boolean, default: false
+
             # VCS information where the Rock release information is stored
             ROCK_RELEASE_INFO = Hash[
                 'github' => 'rock-core/buildconf',
@@ -79,35 +81,55 @@ module Rock
                         "#{release_name} does not look like a valid release name"
                 end
 
-                def match_names_to_packages_in_versions(names, versions, options = Hash.new)
-                    names = names.flat_map do |name|
-                        name = patter_matcher_from_arg(name)
+                def ensure_autoproj_config_loaded
+                    if !@autoproj_config_loaded
+                        tool = Autoproj::CLI::InspectionTool.new(@ws)
+                        tool.initialize_and_load
+                        tool.finalize_setup
+                        @autoproj_config_loaded = true
+                    end
+                end
+
+                def match_names_to_packages_in_versions(names, versions, ignore_missing: false)
+                    unmatched_pkg_name_to_entry = Hash.new
+                    matched_pkg = Hash.new
+                    matched_pkg_set = Array.new
+
+                    names.each do |name|
                         entries = versions.find_all do |vcs|
                             key = vcs.keys.first
                             if key =~ /^pkg_set:/
-                                (name === vcs['name']) || (name === key)
+                                pkg_set_name = $'
+                                if (name === vcs['name']) || (name === key)
+                                    matched_pkg_set << pkg_set_name
+                                end
+                            elsif name === key
+                                matched_pkg[key] = vcs
+                                true
                             else
-                                name === key
+                                unmatched_pkg_name_to_entry[key] = vcs
+                                false
                             end
                         end
 
-                        if entries.empty? && !options[:ignore_missing]
+                        if entries.empty? && !ignore_missing
                             Autoproj.error "cannot find a package or package set matching #{name} in release #{release}"
                             return
                         end
-
-                        entries
                     end
 
-                    pkg_sets, pkgs = names.partition { |vcs| vcs.keys.first =~ /^pkg_set:/ }
-                    if !pkg_sets.empty?
-                        Autoproj.initialize_and_load
-                        pkg_sets.each do |pkg_set_name|
-                            pkg_set = Autoproj.manifest.package_set(pkg_set_name)
-                            pkgs.concat(pkg_set.each_package.to_a)
+                    if !matched_pkg_set.empty?
+                        ensure_autoproj_config_loaded
+                        matched_pkg_set.each do |pkg_set_name|
+                            pkg_set = @ws.manifest.package_set(pkg_set_name)
+                            pkg_set.each_package do |pkg|
+                                if vcs = unmatched_pkg_name_to_entry[pkg.name]
+                                    matched_pkg[vcs.keys.first] = vcs
+                                end
+                            end
                         end
                     end
-                    pkgs
+                    matched_pkg
                 end
             end
 
@@ -142,11 +164,13 @@ module Rock
                 if release_name == "master"
                     FileUtils.rm_f File.join(config_dir, RELEASE_VERSIONS)
                     @ws.config.set("ROCK_SELECTED_FLAVOR", "master")
+                    @ws.config.set('current_rock_release', false)
                     @ws.save_config
                     Autoproj.message "successfully setup flavor #{release_name}"
                 elsif release_name == "stable"
                     FileUtils.rm_f File.join(config_dir, RELEASE_VERSIONS)
                     @ws.config.set("ROCK_SELECTED_FLAVOR", "stable")
+                    @ws.config.set('current_rock_release', false)
                     @ws.save_config
                     Autoproj.message "successfully setup flavor #{release_name}"
                 else
@@ -155,17 +179,20 @@ module Rock
                     File.open(File.join(config_dir, RELEASE_VERSIONS), 'w') do |io|
                         io.write versions
                     end
+                    @ws.config.set('current_rock_release', release_name)
+                    @ws.save_config
                     Autoproj.message "successfully setup release #{release_name}"
                 end
 
                 Autoproj.message "  autoproj status will tell you what has changed"
                 Autoproj.message "  aup --all will attempt to include the new release changes to your working copy"
                 Autoproj.message "  aup --all --reset will (safely) reset your working copy to the release's state"
+                Autoproj.message "  aup --all --force-reset will UNSAFELY reset your working copy to the release's state"
             end
 
-            desc 'freeze-packages NAMES', 'freeze the given package(s) or package set. If a package set is given, its packages are frozen (but not the package set itself, use freeze-package-set for that)'
-            def freeze_packages(*names)
-                release_name = Autoproj.config.get('current_rock_release', false)
+            desc 'freeze NAMES', 'freeze the given package(s) or package set. If a package set is given, its packages are frozen'
+            def freeze(*names)
+                release_name = @ws.config.get('current_rock_release', false)
                 if !release_name
                     Autoproj.error "currently not on any release, use rock-release switch first"
                     return
@@ -175,21 +202,13 @@ module Rock
                 versions = YAML.load(version_file)
 
                 pkgs = match_names_to_packages_in_versions(names, versions)
-
-                to_merge = Hash.new
-                pkgs.each do |pkg_name|
-                    if ver = versions[pkg_name]
-                        to_merge[pkg_name] = ver
-                    end
-                end
-
-                ops = Autoproj::Ops::Snapshot.new(Autoproj.manifest)
-                ops.save_versions(to_merge, File.join(config_dir, RELEASE_VERSIONS))
+                ops = Autoproj::Ops::Snapshot.new(@ws.manifest)
+                ops.save_versions(pkgs.values, File.join(config_dir, RELEASE_VERSIONS), replace: false)
             end
 
-            desc 'unfreeze-packages', 'Allow the given packages or package set to be updated.'
-            def unfreeze
-                release_name = Autoproj.config.get('current_rock_release', false)
+            desc 'unfreeze', 'Allow the given packages or package set to be updated.'
+            def unfreeze(*names)
+                release_name = @ws.config.get('current_rock_release', false)
                 if !release_name
                     Autoproj.error "currently not on any release, use rock-release switch first"
                     return
@@ -203,13 +222,12 @@ module Rock
 
                 versions = YAML.load(File.read(release_versions_path))
                 pkgs = match_names_to_packages_in_versions(names, versions, ignore_missing: true)
-
                 versions.delete_if do |vcs|
-                    pkgs.include?(vcs.keys.first)
+                    pkgs.has_key?(vcs.keys.first)
                 end
 
-                ops = Autoproj::Ops::Snapshot.new(Autoproj.manifest)
-                ops.save_versions(to_merge, release_versions_path, replace: true)
+                ops = Autoproj::Ops::Snapshot.new(@ws.manifest)
+                ops.save_versions(versions, release_versions_path, replace: true)
                 Autoproj.message "updated #{release_versions_path}"
             end
 
